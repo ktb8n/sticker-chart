2c3b994bdf7412baaa91208824776a2935402893
Katie Baeten <katherinebaeten@gmail.com>
2022-06-10 18:45:16 -0500

    create generic component to test functionality
    create article component that returns article plus some props passed in from props.data
    **WHAT IS handle={props.handle}

---
+++ stickerboard.js
@@ -0,0 +1,11 @@
+//This is the article of the holy grail layout. When a sticker is earned (a user enters a new completed task) they are given the option of a set of stickers (the tier their currently at). They can place that sticker anywhere on the sticker wall - they can zoom in and out of the sticker wall, but once it's placed it cannot be moved. The stickers can overlap each other.
+
+function Article(props){
+    return (<>
+        <artcle>
+            <PlusMinus section="article" handle={props.handle}/>
+            <div className="section">Artcle:{props.data.article}</div>
+            <Data data={props.data}/>
+        </artcle>
+    </>);
+}
